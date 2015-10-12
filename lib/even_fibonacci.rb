391b0eab98605d400d8118f17562c91f426fb906
babyshoes <dianecai@gmail.com>
2015-10-12 11:34:59 -0400

    pass go, collect 00

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  sequence = [1, 2]
+  i = 0
+  next_num = sum = 2
+  while next_num < limit
+    next_num = sequence[i] + sequence[i+1]
+    sequence << next_num
+    sum += next_num if next_num<limit && next_num%2==0
+    i += 1
+  end
+  sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+  attr_reader :limit
+    
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    sequence = [1, 2]
+    i = 0
+    next_num = sum = 2
+    while next_num < limit
+      next_num = sequence[i] + sequence[i+1]
+      sequence << next_num
+      sum += next_num if next_num<limit && next_num%2==0
+      i += 1
+    end
+    sum
+  end
+end
